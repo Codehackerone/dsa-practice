8d687d28a5369820b5ec5ec7bf4c7b0ebe630165
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-09-08 18:56:31 +0530

    array codes added

---
+++ practice/Arrays/CPP/swap.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+   int a,b;
+   cin>>a>>b;
+
+   cout<<"before swapping"<<endl;
+   cout<<"a = "<<a<<endl;
+   cout<<"b = "<<b<<endl;
+   swap(a,b);
+    cout<<"after swapping"<<endl;
+   cout<<"a = "<<a<<endl;
+   cout<<"b = "<<b<<endl;
+
+
+
+   return 0;
+}
