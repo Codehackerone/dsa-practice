4faa91005160df51ef43efaaabb109e76c59f6ce
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-12-02 19:35:19 +0530

    selection  added

---
+++ College/Assignments/Assignment2/selectionsort.c
@@ -0,0 +1,34 @@
+// program to perform selection sort
+#include <stdio.h>
+#include <stdlib.h>
+
+void selection_sort(int *arr, int n);
+
+int main()
+{
+    int n, min_index, temp;   
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)scanf("%d", &arr[i]);
+    selection_sort(arr, n);
+    printf("The sorted array is: ");
+    for (int i = 0; i < n; i++)printf("%d ", arr[i]); 
+    return 0;
+}
+
+void selection_sort(int *arr, int n){
+    int min_index, temp;
+    for(int i=0; i<n-1; i++){
+        min_index = i;
+        for(int j=i+1; j<n; j++){
+            if(arr[j] < arr[min_index]){
+                min_index = j;
+            }
+        }
+        temp = arr[i];
+        arr[i] = arr[min_index];
+        arr[min_index] = temp;
+    }
+}
