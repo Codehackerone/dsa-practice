49d773ec3b237e1bf744549461be41342e61eebf
Soumyajit Datta <soumyajitdatta123@gmail.com>
2021-12-06 15:48:00 +0530

    merge sort added

---
+++ College/Assignments/Assignment2/mergesort.c
@@ -0,0 +1,43 @@
+//program to perform merge sort
+#include<stdio.h>
+#include<stdlib.h>
+
+void merge_sort(int *arr, int n);
+
+int main()
+{
+    int n, min_index, temp;   
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("Enter the elements of the array: ");
+    for (int i = 0; i < n; i++)scanf("%d", &arr[i]);
+    merge_sort(arr, n);
+    printf("The sorted array is: ");
+    for (int i = 0; i < n; i++)printf("%d ", arr[i]); 
+    return 0;
+}
+
+void merge_sort(int *arr, int n)
+{
+    if (n > 1)
+    {
+        int mid = n / 2;
+        int *left = (int *)malloc(mid * sizeof(int));
+        int *right = (int *)malloc((n - mid) * sizeof(int));
+        for (int i = 0; i < mid; i++)left[i] = arr[i];
+        for (int i = mid; i < n; i++)right[i - mid] = arr[i];
+        merge_sort(left, mid);
+        merge_sort(right, n - mid);
+        int i = 0, j = 0, k = 0;
+        while (i < mid && j < n - mid)
+        {
+            if (left[i] < right[j])arr[k++] = left[i++];
+            else arr[k++] = right[j++];
+        }
+        while (i < mid)arr[k++] = left[i++];
+        while (j < n - mid)arr[k++] = right[j++];
+        free(left);
+        free(right);
+    }
+}
